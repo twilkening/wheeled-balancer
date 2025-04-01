bce80f5009c4c90fc8aac084f94122d882faf691
Teo Wilkening <teo.wilkening@gmail.com>
2025-03-31 20:08:38 -0700

    rk4 simulation working!

--- modeling/cpp_cart_pend_sim/src/main.cpp
+++ modeling/cpp_cart_pend_sim/src/main.cpp
@@ -1,40 +1,38 @@
-/*In order to actually simulate the file... 
-it's really not that hard to implement the RK4 is it?
-
-equation... sin(x) 
-
-let's see if we can do this!*/
-
 #include "rk4.cpp"
 #include <cmath>
 #include <array>
 
-std::array<double, 2> f(double t, const double x[2]) {
-    // ODE: 
-    // dx1/dt = wn
-    // dx2/dt = -wc**2 * sin(theta) - 2*gamma*wn
-    // where wc = 1.0, gamma = 0.1, wn = 1.0
-    // and theta = x1, wn = x2
-    // This is a simple pendulum with damping
+// ODE: 
+// dx0/dt = wn
+// dx1/dt = -wc**2 * sin(theta) - 2*gamma*wn
+// where wc = 1.0, gamma = 0.1, wn = 1.0
+// and theta = x0, wn = x1
+// This is a simple pendulum with damping
+double f0(double t, double x0, double x1) {
+    return x1; // return the derivatives as an array
+}
 
-    double wc = 1.0; // natural frequency
-    double gamma = 0.1; // damping coefficient
-    double f1 = x[1]; 
-    double f2 = -wc * wc * sin(x[0]) - 2 * gamma * x[1];
-    return {f1, f2}; // return the derivatives as an array
+double f1(double t, double x0, double x1) {
+    double g = 9.81; // [m/s^2] gravitational acceleration
+    double l = 1.0; // [m] length of the pendulum
+    double wc = std::sqrt(g/l); // natural frequency
+    double beta = 0.1; // damping ratio
+    double m = 1.0; // [kg] mass of the pendulum bob
+    double gamma = beta/(2 * m * l); // damping coefficient
+
+    return  -wc * wc * sin(x0) - 2 * gamma * x1;
 }
 
 int main()
 {
-    // Define the ODE function
-    // dx/dt = f(t, x)  where f is a function of t and x
-    // For example, let's use the ODE dx/dt = sin(x)
-
+    // Define the ODE parameters
     double t0 = 0.0; // initial time
-    double t_end = 10.0; // end time
+    double t_end = 1.0; // end time
     double h = 0.01; // step size
+    const double pi = 3.141592653589793;
+    double xinit[2] = {pi/6, 0}; // initial conditions [theta, wn]
 
-    rk4 solver(f(), t0, t_end, h); // create RK4 solver instance
+    rk4 solver(f0, f1, xinit, t0, t_end, h); // create RK4 solver instance
     solver.run(); // run the solver
     
     return 0;
--- modeling/cpp_cart_pend_sim/src/rk4.cpp
+++ modeling/cpp_cart_pend_sim/src/rk4.cpp
@@ -1,6 +1,10 @@
 #include <cmath>
 #include <vector>
+#include <array>
+#include <iostream>
 
+// RK4 class for solving ODEs using the Runge-Kutta 4th order method
+// This class is designed to solve a system of two first-order ODEs
 class rk4
 {
     double xn[2]; // current states
@@ -8,51 +12,65 @@ class rk4
     const double h; // step size
     double t; // current time
     double t_end; // end time
-    double (*f)(double, double); // function pointer for the ODE
+    double (*f0)(double, double, double); // function pointer for the ODE0
+    double (*f1)(double, double, double); // function pointer for the ODE1
 public:
-    rk4(double (*f)(double, double), double xinit[2], double t0, double t_end, double h)
-        : f(f), t(t0), t_end(t_end), h(h)
+    rk4(double (*f0)(double, double, double), double (*f1)(double, double, double),
+        double xinit[2], double t0, double t_end, double h)
+        : f0(f0), f1(f1), t(t0), t_end(t_end), h(h)
     {
         // Initialize the states
         xn[0] = xinit[0];
         xn[1] = xinit[1];
+        std::cout << "h: " << h << std::endl;
+        std::cout << "t_end: " << t_end << std::endl;
+        std::cout << "t: " << t << ", x0: " << xn[0] << ", x1: " << xn[1] << std::endl;
+        std::cout << "f0: " << f0(t, xn[0], xn[1]) << std::endl;
+        std::cout << "f1: " << f1(t, xn[0], xn[1]) << std::endl;
+        std::cout << "----------------------------------------" << std::endl;
+        std::cout << "----------------------------------------" << std::endl;
     }
+    void step(); // perform a single RK4 step
+    bool isComplete() const; // check if the simulation is complete
+    void run(); // run the RK4 solver
+    std::array<double, 2> getState() const; // get the current state
+};
 
-    void step()
-    {
-        k1[0] = h * f(t, xn[0]);
-        k1[1] = h * f(t, xn[1]);
+void rk4::step()
+{
+    k1[0] = h * f0(t, xn[0], xn[1]);
+    k1[1] = h * f1(t, xn[0], xn[1]);
 
-        k2[0] = h * f(t + h / 2, xn[0] + k1[0] / 2);
-        k2[1] = h * f(t + h / 2, xn[1] + k1[1] / 2);
+    k2[0] = h * f0(t + h / 2, xn[0] + k1[0] / 2, xn[1] + k1[1] / 2);
+    k2[1] = h * f1(t + h / 2, xn[0] + k1[0] / 2, xn[1] + k1[1] / 2);
 
-        k3[0] = h * f(t + h / 2, xn[0] + k2[0] / 2);
-        k3[1] = h * f(t + h / 2, xn[1] + k2[1] / 2);
+    k3[0] = h * f0(t + h / 2, xn[0] + k2[0] / 2, xn[1] + k2[1] / 2);
+    k3[1] = h * f1(t + h / 2, xn[0] + k2[0] / 2, xn[1] + k2[1] / 2);
 
-        k4[0] = h * f(t + h, xn[0] + k3[0]);
-        k4[1] = h * f(t + h, xn[1] + k3[1]);
+    k4[0] = h * f0(t + h, xn[0] + k3[0], xn[1] + k3[1]);
+    k4[1] = h * f1(t + h, xn[0] + k3[0], xn[1] + k3[1]);
 
-        xn[0] += (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]) / 6;
-        xn[1] += (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1]) / 6;
+    xn[0] += (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]) / 6;
+    xn[1] += (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1]) / 6;
 
-        t += h;
+    t += h;
+}
+
+bool rk4::isComplete() const
+{
+    return t >= t_end;
+}
+
+void rk4::run()
+{
+    while (t < t_end) {
+        step();
+        std::cout << "t: " << t << ", x0: " << xn[0] << ", x1: " << xn[1] << std::endl;
+        // You can also store the results in a vector or array if needed
     }
+}
 
-    bool isComplete() const
-    {
-        return t >= t_end;
-    }
-
-    void run()
-    {
-        while (t < t_end) {
-            step();
-        }
-    }
-
-    std::vector<double> getState() const
-    {
-        return {xn[0], xn[1]};
-    }
-
+std::array<double, 2> rk4::getState() const
+{
+    return {xn[0], xn[1]};
 }
